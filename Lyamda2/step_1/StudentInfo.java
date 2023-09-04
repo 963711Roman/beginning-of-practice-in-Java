cffe1d537236e0804bc395955660d3d04d2bb6ae
963711Roman <142265876+963711Roman@users.noreply.github.com>
2023-09-04 21:59:19 +0300

    Add files via upload

---
+++ Lyamda2/step_1/StudentInfo.java
@@ -0,0 +1,77 @@
+package Lyamda2.step_1;
+
+import java.util.ArrayList;
+
+public class StudentInfo {
+void testStudent(ArrayList<Student> al, StudentChecks sc){
+    for (Student s : al){
+        if (sc.check(s)){
+            System.out.println(s);
+        }
+    }
+}
+/*
+    void  printStudentOverGrade(ArrayList<Student> al, double grade){
+for (Student s : al){
+    if (s.avgGrade > grade){
+        System.out.println(s);
+    }
+}
+    }
+    void  printStudentUnderAge(ArrayList<Student> al, int age){
+        for (Student s : al){
+            if (s.age > age){
+                System.out.println(s);
+            }
+        }
+    }
+    void  printStudentMixCondition(ArrayList<Student> al, int age, double grade, char sex ){
+        for (Student s : al){
+            if (s.age > age && s.avgGrade > grade && s.sex == sex ){
+                System.out.println(s);
+            }
+        }
+    }
+*/
+}
+
+class  Test{
+    public static void main(String[] args) {
+        Student st1 = new Student("Ivan", 'm', 22, 3, 8.3);
+        Student st2 = new Student("Nikolay", 'm', 28, 2, 6.4);
+        Student st3 = new Student("Elena", 'f', 19, 1, 8.9);
+        Student st4 = new Student("Petr", 'm', 35, 4, 7);
+        Student st5 = new Student("Maria", 'f', 23, 3, 9.1);
+        ArrayList<Student> students =new ArrayList<>();
+        students.add(st1);
+        students.add(st2);
+        students.add(st3);
+        students.add(st4);
+        students.add(st5);
+        StudentInfo info = new StudentInfo();
+        //info.testStudent(students, new CheckOverGrade());
+       // info.printStudentOverGrade(students, 8);
+        System.out.println("-----------------------");
+        info.testStudent(students, new StudentChecks() {
+            // так краще прописувати реалізацію відразу там де визиваємо інтрфейс тобто
+            // робимо анонімний клас просто не треба створювати таке що ми використаємо всьоо один раз
+            @Override
+            public boolean check(Student s) {
+                return s.age < 30  ;
+            }
+        });
+        //info.printStudentUnderAge(students, 30);
+        System.out.println("-----------------------");
+        //info.printStudentMixCondition(students, 20, 9.5, 'f' );
+    }
+}
+// так було прописали реалізацію інетерфейсу і визиваємо клас в якому вона прописана
+interface StudentChecks{
+    boolean check(Student s);
+}
+// class CheckOverGrade implements StudentChecks {
+   // @Override
+    // public boolean check(Student s) {
+      //  return s.avgGrade > 8 ;
+    //}
+//}
